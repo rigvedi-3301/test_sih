97a8b1816e55a9d52fc7c39d9c8406e2846823ba
Hritaansh Mehra <hritaansh.mehra@gmail.com>
2025-10-14 18:49:15 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -64,11 +64,6 @@ test_urls = [
     "http://203.0.113.77/installer/latest_installer.zip?payload=cmd.exe",
     "https://www.example.com/%2e%2e/%2e%2e/admin/config.php",
     "https://login.example.com/?user=admin&pass=%3Cscript%3Ealert(1)%3C%2Fscript%3E",
-    "https://google.com",
-    "https://free-download-malware.ru",
-    "https://www.bankofamerica.com/login",
-    "http://bit.ly/2FakeLink",
-    "http://192.168.1.1/admin"
 ]
 
 print("🔄 Tokenizing URLs...")
